0974cd2a6b29125fc0c1c14562455f6f433b319f
mashukmashum <mashukgce@gmail.com>
2018-10-26 19:22:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+import subprocess
+
+@app.route('/')
+def index():
+    
+    q = request.args.get('q')
+    if q <> None:
+        p = subprocess.Popen(['bash', 'run.sh', q], stdout=subprocess.PIPE)
+        output, err = p.communicate()
+        output = output.replace('\n','<br>')
+    else:
+        output = '<a href="https://aiphoto.herokuapp.com/?q=https://demo.phpgang.com/crop-images/demo_files/pool.jpg">Tiger Example</a>'
+
+    return output
